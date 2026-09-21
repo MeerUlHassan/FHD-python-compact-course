79b95654b6e4b47aef5ba36352073a6ff562e5f1
MeerUlHassan <github4421@gmail.com>
2026-09-21 12:28:17 +0500

    Week 1 Task 3: Conversions

---
+++ week1/task3_conversion.py
@@ -0,0 +1,20 @@
+
+zahl = 10
+kommazahl = float(zahl)
+print('Floating value is',kommazahl)
+
+float_num = 8.64
+int_num = int(float_num)
+print('Int value is', int_num)
+
+num = 100
+str_num = str(num)
+print('String value is', str_num)
+
+string = "20"
+int_num = int(string)
+print('int value is ', int_num)
+
+bool_num = 10
+result = bool(bool_num)
+print(result)
