dd4e37046a818002f790c47189a0d7c6dd2f21c7
MeerUlHassan <github4421@gmail.com>
2026-09-21 12:14:33 +0500

    Week 1 Task 2: variables and types

---
+++ week1/task2_variables.py
@@ -0,0 +1,16 @@
+# Create a program that stores an integer, a floating-point number, a string, and a boolean  
+# variable, and then outputs them. 
+
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print(zahl)
+print("Type of zahl:",type(zahl))
+print(kommazahl)
+print("Type of kommazahl:",type(kommazahl))
+print(text)
+print("Type of text:",type(text))
+print(wahrheitswert)
+print("Type of wahrheitswert:",type(wahrheitswert))
